145e0121fd98c4e411acf0e0ef191c736ee8f3ab
TheSnarkHunters <132657852+TheSnarkHunters@users.noreply.github.com>
2026-05-11 12:07:37 +0200

    Delete pizzeria directory

--- pizzeria/navbar.php
+++
@@ -1,7 +0,0 @@
-<nav>
-    <a href="http://localhost/siti/ristorante/index.php">Home</a>
-    <a href="http://localhost/siti/ristorante/crud/lista.php">Prodotti</a>
-    <a href="http://localhost/siti/ristorante/crud/inserisci.php">Inserisci</a>
-    <a href="http://localhost/siti/ristorante/crud/statistica.php">Statistica</a>
-    <a href="http://localhost/siti/ristorante/auth/logout.php">Logout</a>
-</nav>
